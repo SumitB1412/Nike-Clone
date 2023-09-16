e299137874e8317c44403232dcf6e0b0aa4c0d25
SumitB1412 <beniwalsumit1412@gmail.com>
2023-09-16 16:12:56 +0530

    change base url

--- client/src/Redux/products/actions.js
+++ client/src/Redux/products/actions.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 import { GET_PRODUCTS, GET_SINGLE_PRODUCT } from "./actionTypes";
+import { baseURL } from "../../baseURL";
 
 export const getAllProducts = async (dispatch) => {
-  let res = await axios.get("http://localhost:7000/products/getAll");
+  let res = await axios.get(`${baseURL}/products/getAll`);
   dispatch({ type: GET_PRODUCTS, payload: res.data.data });
 };
 
-export const getSingleProduct = async (dispatch,productID) => {
-  let res = await axios.get(`http://localhost:7000/products/get-single-product/${productID}`);
-  dispatch({type:GET_SINGLE_PRODUCT,payload:res.data.product})
-}
+export const getSingleProduct = async (dispatch, productID) => {
+  let res = await axios.get(
+    `${baseURL}/products/get-single-product/${productID}`
+  );
+  dispatch({ type: GET_SINGLE_PRODUCT, payload: res.data.product });
+};
