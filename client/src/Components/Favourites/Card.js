ab04270e8f47c950e0ae46e4331e2de6646eb555
Anuj Choudhary <abchoudhary13@gmail.com>
2023-09-16 12:01:25 +0530

    change products detail

--- client/src/Components/Favourites/Card.js
+++ client/src/Components/Favourites/Card.js
@@ -9,14 +9,14 @@ const Card = ({product}) => {
         </div></Link>
         <div className='flex pt-4 w-full text-base justify-between'>
         <Link to={'/products/description/:id'}><div>
-                <p>{product.title}</p>
+                <p className='max-w-[25ch] truncate'>{product.title}</p>
             </div></Link>
             <div>
                 <p>₹{product.price}</p>
             </div>
         </div>
         <div>
-            <p className='text-gray-400'>{product.description}</p>
+            <p className='text-gray-400 max-w-[27ch] truncate'>{product.description}</p>
         </div>
         <div className='py-4'>
             <button className='px-4 py-2 border border-gray-300 hover:border-gray-700 rounded-full'>Remove</button>
--- client/src/Components/navbar/NavBarDesktop.js
+++ client/src/Components/navbar/NavBarDesktop.js
@@ -1,18 +1,17 @@
 import React from "react";
-import {Link,useNavigate} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import nike from "./nike.jpg";
 import { AiOutlineHeart } from "react-icons/ai";
 import { BiShoppingBag } from "react-icons/bi";
 
 const NavBarDesktop = () => {
-  const navigate = useNavigate();
   return (
     <div className="hidden md:flex w-full justify-between items-center h-16 px-1">
       <div>
         <Link to={'/'}><img src={nike} alt="logo" className="h-[60px] px-4" /></Link>
       </div>
       <ul className="hidden md:flex">
-        <li className="p-4 relative after:absolute after:w-0 hover:after:w-full after:h-[2px] after:left-0 after:bottom-0 after:bg-black cursor-pointer"><Link to={'/products'}>New & Featured</Link></li>
+        <li className="p-4 relative after:absolute after:w-0 hover:after:w-full after:h-[2px] after:left-0 after:bottom-0 after:bg-black cursor-pointer"><Link to={'/'}>New & Featured</Link></li>
         <li className="p-4 relative after:absolute after:w-0 hover:after:w-full after:h-[2px] after:left-0 after:bottom-0 after:bg-black cursor-pointer"><Link to={'/products'}>All Products</Link></li>
         <li className="p-4 relative after:absolute after:w-0 hover:after:w-full after:h-[2px] after:left-0 after:bottom-0 after:bg-black cursor-pointer"><Link to={'/products'}>Men</Link></li>
         <li className="p-4 relative after:absolute after:w-0 hover:after:w-full after:h-[2px] after:left-0 after:bottom-0 after:bg-black cursor-pointer"><Link to={'/products'}>Women</Link></li>
